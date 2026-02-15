88edcb02771d53a68352511ca49a57d2c36623fe
garystarr-surgi <gary.starr@surgishop.com>
2026-02-15 14:09:53 -0500

    Create Purchase_Receipt_allow_blemish_in_received_qty.py

---
+++ server_scripts/doctype/Purchase_Receipt_allow_blemish_in_received_qty.py
@@ -0,0 +1,26 @@
+def execute(doc, method=None):
+    flt = frappe.utils.flt
+
+    total_blemish = 0.0
+    total_rejected = 0.0
+    total_received = 0.0
+
+    for item in doc.items:
+        qty_ordered = flt(item.get("qty") or 0)
+        blemish = flt(item.get("custom_blemish_quantity") or 0)
+        rejected = flt(item.get("rejected_qty") or 0)
+
+        accepted = qty_ordered + blemish
+        received = accepted + rejected
+
+        item.qty = received
+        item.accepted_qty = accepted
+        item.received_qty = received
+
+        total_blemish += blemish
+        total_rejected += rejected
+        total_received += received
+
+    doc.custom_total_blemish_quantity = total_blemish
+    doc.custom_total_rejected_quantity = total_rejected
+    doc.custom_total_received = total_received
